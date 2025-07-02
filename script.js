6421f2581637c1132cd88400a6ba974ae1b55b6f
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-02 12:21:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//script file or javascript
+//script file or javascript 
 const navbarShowBtn = document.querySelector('.navbar-show-btn');
 const navbarCollapseDiv = document.querySelector('.navbar-collapse');
 const navbarHideBtn = document.querySelector('.navbar-hide-btn');
